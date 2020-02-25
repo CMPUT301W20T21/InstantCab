e46d695a2f57c6c680b22a9c9371b042da09c925
PeiyuanBenChen <43076353+PeiyuanBenChen@users.noreply.github.com>
2020-02-25 16:28:52 -0700

    push InstantCab android studio project

---
+++ InstantCab/app/src/main/java/com/example/instantcab/SignupViewActivity.java
@@ -0,0 +1,27 @@
+package com.example.instantcab;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+public class SignupViewActivity extends AppCompatActivity {
+    private FirebaseAuth mAuth;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_signup_view);
+        mAuth = FirebaseAuth.getInstance();
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+
+    }
+
+}
