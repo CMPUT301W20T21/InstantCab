b409a684ab22a7d5caeef1d102d14683f837e86f
LijiangnanTian <46389083+LijiangnanTian@users.noreply.github.com>
2020-03-03 14:36:49 -0700

    Add files via upload

---
+++ InstantCab/InstantCab/app/src/main/java/com/example/instantcab/MainActivity.java
@@ -0,0 +1,41 @@
+package com.example.instantcab;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+public class MainActivity extends AppCompatActivity {
+    private Button mBtnSignup;
+    private Button mBtnLogin;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mBtnSignup = findViewById(R.id.Signup);
+        mBtnLogin = findViewById(R.id.Login);
+        mBtnSignup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //jump to sign up activity
+                Intent intent = new Intent(MainActivity.this, SignupViewActivity.class);
+                startActivity(intent);
+            }
+        });
+        mBtnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // jump to login activity
+                Intent intent = new Intent(MainActivity.this, LoginViewActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
---
+++ InstantCab/InstantCab/app/src/main/java/com/example/instantcab/PaymentActivity.java
@@ -0,0 +1,58 @@
+package com.example.instantcab;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.text.Editable;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class PaymentActivity extends AppCompatActivity {
+    private Button ButtonPay;
+    private Button ButtonTip;
+    private TextView totalCost;
+    private TextView showFare;
+    private EditText tipAmount;
+    private String fare;
+    private float tip;
+    private float total;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_payment);
+        fare = getIntent().getStringExtra("FARE");
+
+        ButtonPay = findViewById(R.id.pay);
+        ButtonTip = findViewById(R.id.tip);
+        totalCost = findViewById(R.id.total_cost);
+        tipAmount = findViewById(R.id.tip_amount);
+        // show the fare
+        showFare = findViewById(R.id.fare);
+        showFare.setText("$"+fare);
+        totalCost.setText(String.format(getString(R.string.total), Float.parseFloat(fare)));
+
+        ButtonTip.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Click to tip
+                tip = Float.parseFloat((tipAmount.getText().toString()));
+                total = tip + Float.parseFloat(fare);
+                totalCost.setText(String.format(getString(R.string.total), total));
+            }
+        });
+
+        ButtonPay.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Click to pay
+                Intent intent = new Intent(PaymentActivity.this, anotherPaymentActivity.class);
+                intent.putExtra("FARE", total);
+                startActivity(intent);
+            }
+        });
+    }
+}
