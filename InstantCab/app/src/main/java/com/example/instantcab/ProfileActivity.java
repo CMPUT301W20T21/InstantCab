c5ea4d0445e054b770cf78a809b36577b6696958
Haochen Gou <32112948+Haochen-098@users.noreply.github.com>
2020-03-13 00:24:50 -0600

    Update ProfileActivity.java

--- InstantCab/app/src/main/java/com/example/instantcab/ProfileActivity.java
+++ InstantCab/app/src/main/java/com/example/instantcab/ProfileActivity.java
@@ -1,3 +1,17 @@
+/**Copyright 2020 CMPUT301W20T21
+
+ Licensed under the Apache License, Version 2.0 (the "License");
+ you may not use this file except in compliance with the License.
+ You may obtain a copy of the License at
+
+ http://www.apache.org/licenses/LICENSE-2.0
+
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and
+ limitations under the License.*/
+
 package com.example.instantcab;
 
 import android.app.Dialog;
@@ -23,6 +37,11 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+/**
+ * This activity is for profile, display different profile UI for diver and user
+ * can edit the email and phone number on profile
+ * @author hgou
+ */
 
 public class ProfileActivity extends AppCompatActivity implements View.OnClickListener {
     private Toolbar toolbar;
